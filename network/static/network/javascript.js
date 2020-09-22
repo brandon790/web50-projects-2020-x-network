6acb6b2aa707df8c83e7a50f4e5d3250991b6dd0
brandon790 <67201798+brandon790@users.noreply.github.com>
2020-09-22 10:21:58 -0400

    partial edit

--- network/static/network/javascript.js
+++ network/static/network/javascript.js
@@ -1,10 +1,22 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     
-    document.getElementById("save_edit_{{ item.id }}").addEventListener("click", test_alert, false);
-    function test_alert() {
+    document.querySelector("#save_edit").addEventListener('click', function() {
+        var x = document.getElementById('edited_content').value;
+        alert(x);
+    });
+
+/*     function save_edit() {
         alert("yes");
-        
+        var x = document.getElementById("post_{{ item.id }}").value;
+                var update_save = {
+                    user = "{{ item.user }}",
+                    content = x,
+                    timestamp = "{{ item.timestamp }}",
+                    likes = "{{ item.likes }}"};
+                    update_save.save();
+                } */
+
     document.querySelector('#all-posts').addEventListener('click', function() { 
     document.querySelector('#profile').style.display = 'none'; 
     document.querySelector('#following').style.display = 'none';                        
